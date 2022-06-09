3161f3dae537ea9aabf0dc8f53239244ca6ee4c8
Rafael-Nicolau <nicolaurafael@hotmail.com>
2022-06-09 11:39:00 +0200

    Add timeout to Alert add Reducer functions for display and clear the alert

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,4 +1,7 @@
+import { useAppContext } from "../context/appContext";
+
 const Alert = () => {
-  return <div className="alert alert-danger">Alert goes Here</div>;
+  const { alertType, alertText } = useAppContext();
+  return <div className={`alert alert-${alertType}`}>{alertText}</div>;
 };
 export default Alert;
