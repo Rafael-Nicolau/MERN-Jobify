c7dc84bcacaa0d8713a9a4a4b4752c042dc85539
Rafael-Nicolau <nicolaurafael@hotmail.com>
2022-06-13 15:14:34 +0200

    Add more error handlers for bad_request, not_found and Bad_gateway

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,32 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
+
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
