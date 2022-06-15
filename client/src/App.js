8d6428a0a0a9842a3d716e04151c6dd99057e8be
Rafael-Nicolau <nicolaurafael@hotmail.com>
2022-06-15 18:31:51 +0200

    Create components page for Dashboard view

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard, Register, Error, Landing } from "./pages";
-
+import { Register, Error, Landing } from "./pages";
+import {
+  Stats,
+  SharedLayout,
+  Profile,
+  AllJobs,
+  AddJob,
+} from "./pages/dashboard";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<div>DashBoard</div>} />
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
