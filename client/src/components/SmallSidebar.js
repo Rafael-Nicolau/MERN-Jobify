ca6df877375664be84d24daa4378d34311463f00
Rafael-Nicolau <nicolaurafael@hotmail.com>
2022-06-18 11:41:16 +0200

    Refactor -> Create NavLinks as independent component
    Navlinks will be used in Small and Big Sidebar components imported and with props

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -4,6 +4,7 @@ import { useAppContext } from "../context/appContext";
 import links from "../utils/links";
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
   const { showSidebar, toggleSidebar } = useAppContext();
@@ -21,24 +22,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, id, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  key={id}
-                  onClick={toggleSidebar}
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active" : "nav-link"
-                  }
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggleSidebar} />
         </div>
       </div>
     </Wrapper>
